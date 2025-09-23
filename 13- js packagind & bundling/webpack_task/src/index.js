f7f26131d3556f7fcd9ec9b23579bb4f5a888df5
Yomna Elshorbagy <yumnamohamed30@gmail.com>
2025-09-23 09:07:00 +0300

    adding bundling tools lab2

---
+++ 13- js packagind & bundling/webpack_task/src/index.js
@@ -0,0 +1,12 @@
+import hello from "./hello world"
+hello()
+
+
+
+import header from "./component/header/header"
+
+import "./style.css"
+
+import cover from"./component/cover/cover"
+
+import bio from "./component/bio/bio"
